706ae17ebfa9fdd1dea09db8662ff5d1f03999e3
fsarachu <fsarachu@gmail.com>
2017-06-02 23:39:15 -0300

    Imports optimized

--- index.ios.js
+++ index.ios.js
@@ -5,7 +5,7 @@
  */
 
 import React, {Component} from "react";
-import {AppRegistry, StyleSheet, Text, View} from "react-native";
+import {AppRegistry, StyleSheet, View} from "react-native";
 import Greeting from "./app/components/Greeting";
 
 export default class ReactNativeSandbox extends Component {
