899ff4863a4e226a5c84875768957d8665b49664
fsarachu <fsarachu@gmail.com>
2017-06-02 23:35:44 -0300

    Simple Greeting component using props

---
+++ app/components/Greeting.js
@@ -0,0 +1,14 @@
+/* @flow */
+
+import React, {Component} from 'react';
+import {
+    Text,
+} from 'react-native';
+
+export default class Greeting extends Component {
+    render() {
+        return (
+            <Text>Hello {this.props.name}!</Text>
+        );
+    }
+}
