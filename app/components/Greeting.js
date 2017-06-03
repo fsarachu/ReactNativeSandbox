484451a47444ab3db09c50c010a3e941f9db7c30
fsarachu <fsarachu@gmail.com>
2017-06-02 23:36:01 -0300

    Reformat file

--- app/components/Greeting.js
+++ app/components/Greeting.js
@@ -1,9 +1,7 @@
 /* @flow */
 
-import React, {Component} from 'react';
-import {
-    Text,
-} from 'react-native';
+import React, {Component} from "react";
+import {Text} from "react-native";
 
 export default class Greeting extends Component {
     render() {
