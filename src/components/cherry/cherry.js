fbe58c31f6e20c19c4b2c075c85a46af7dbef864
fsarachu <fsarachu@gmail.com>
2017-06-29 03:19:31 -0300

    Pass source prop from variable

--- src/components/cherry/cherry.js
+++ src/components/cherry/cherry.js
@@ -5,8 +5,10 @@ import {Image} from "react-native";
 
 export default class cherry extends Component {
     render() {
+        let cherry = require('./img/cherry-250x250.png');
+
         return (
-            <Image source={require('./img/cherry-250x250.png')} style={{width: 250, height: 250}}/>
+            <Image source={cherry} style={{width: 250, height: 250}}/>
         );
     }
 }
