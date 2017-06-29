41754da2bde5df89df0b34894ce659ab26a9f49d
fsarachu <fsarachu@gmail.com>
2017-06-29 19:17:50 -0300

    Hide StackNavigator header

--- src/app.js
+++ src/app.js
@@ -3,9 +3,13 @@ import {StackNavigator} from "react-navigation";
 import LoginScreen from "./screens/login";
 import ProfileScreen from "./screens/profile";
 
-const App = StackNavigator({
-    Home: {screen: LoginScreen},
-    Profile: { screen: ProfileScreen },
-});
+const App = StackNavigator(
+    {
+        Home: {screen: LoginScreen},
+        Profile: {screen: ProfileScreen},
+    },
+    {
+        headerMode: 'none',
+    });
 
 export default App;
