cd3bcd041023958f8a5c09d51b2e56d4a036cae1
fsarachu <fsarachu@gmail.com>
2017-06-29 16:26:38 -0300

    Use Login view component stylesheet

--- src/views/login/index.js
+++ src/views/login/index.js
@@ -2,6 +2,7 @@
 
 import React, {Component} from "react";
 import {Text, View} from "react-native";
+import styles from "./styles";
 
 export default class Login extends Component {
     static navigationOptions = {
@@ -10,7 +11,7 @@ export default class Login extends Component {
 
     render() {
         return (
-            <View>
+            <View style={styles}>
                 <Text>Login Screen</Text>
             </View>
         );
