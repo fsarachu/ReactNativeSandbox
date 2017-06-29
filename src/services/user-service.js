b2c26ac4472571cf9412a858c330766cdbe3e6b4
fsarachu <fsarachu@gmail.com>
2017-06-29 16:31:24 -0300

    Implement a basic User Service (not tested, hope it works)

---
+++ src/services/user-service.js
@@ -0,0 +1,14 @@
+import {AsyncStorage} from "react-native";
+
+class UserService {
+
+    setUser(user) {
+        return AsyncStorage.setItem('user', JSON.stringify(user));
+    }
+
+    getUser() {
+        let getUserJSON = AsyncStorage.getItem('user');
+        return getUserJSON.then((json) => json ? JSON.parse(json) : null);
+    }
+
+}
