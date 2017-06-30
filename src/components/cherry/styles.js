8f6f2e642959ad52fa8cfc1a0a1f9ca82b3ad1a4
fsarachu <fsarachu@gmail.com>
2017-06-29 21:29:29 -0300

    Update Cherry component style

--- src/components/cherry/styles.js
+++ src/components/cherry/styles.js
@@ -2,15 +2,12 @@ import {StyleSheet} from "react-native";
 
 export default StyleSheet.create({
     container: {
-        flex: 2,
         margin: 15,
-        alignSelf: 'stretch',
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#829aee',
     },
     content: {
-        width: 250,
-        height: 250,
+        width: 100,
+        height: 100,
     },
 });
