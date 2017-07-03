5279084d3ed82c1e7a04d245eded87b6918e85e6
fsarachu <fsarachu@gmail.com>
2017-07-03 10:43:22 -0300

    Add actions to Counter component

--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -1,15 +1,18 @@
 /* @flow */
 
 import React, {Component} from "react";
-import {Text, View} from "react-native";
+import {Button, Text, View} from "react-native";
 import styles from "./styles";
 
 export default class Counter extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.counterDisplay}>0</Text>
+                <Text style={styles.counterDisplay}>{this.props.count}</Text>
                 <Text style={styles.text}>This is the counter component</Text>
+                <Button title="INCREMENT" onPress={this.props.increment}/>
+                <Button title="DECREMENT" onPress={this.props.decrement}/>
+                <Button title="RESET" onPress={this.props.reset}/>
             </View>
         );
     }
