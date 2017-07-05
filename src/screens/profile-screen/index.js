aad7b04d034a0c9977a43546f160ce3813a64b6c
fsarachu <fsarachu@gmail.com>
2017-07-05 19:29:50 -0300

    Delete ProfilePicturesScreen

--- src/screens/profile-screen/index.js
+++ src/screens/profile-screen/index.js
@@ -3,15 +3,13 @@
 import React from "react";
 import {TabNavigator} from "react-navigation";
 import ProfileInfoScreen from "./info";
-import ProfilePicturesScreen from "./pictures";
 
 const ProfileScreen = TabNavigator({
     Info: {screen: ProfileInfoScreen},
-    Pictures: {screen: ProfilePicturesScreen},
 });
 
 ProfileScreen.navigationOptions = ({navigation}) => ({
-    title: `${navigation.state.params.name}'s Profile`,
+    title: `Profiles`,
 });
 
 export default ProfileScreen;
