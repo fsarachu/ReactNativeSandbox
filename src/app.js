3ca9dda03390534a752f405aa39f36c4d7b7d363
fsarachu <fsarachu@gmail.com>
2017-07-05 19:24:50 -0300

    App renders AppNavigator

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 import React from "react";
-import {Provider} from 'react-redux';
-import store from './store/store';
-import CounterContainer from "./containers/CounterContainer";
+import AppNavigator from "./components/app-navigator";
 
 class App extends React.Component {
     render() {
         return (
-            <Provider store={store}>
-                <CounterContainer/>
-            </Provider>
+            <AppNavigator/>
         );
     }
 }
