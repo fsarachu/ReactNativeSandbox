46565a869839b0889472bcf23639c11ea9a1b1ae
fsarachu <fsarachu@gmail.com>
2017-07-05 19:57:59 -0300

    Wrap tabNavigator inside a component to pass user param as a screenProp

--- src/screens/profile-screen/index.js
+++ src/screens/profile-screen/index.js
@@ -5,13 +5,26 @@ import {TabNavigator} from "react-navigation";
 import ProfileLocalScreen from "./local";
 import ProfileFacebookScreen from "./facebook";
 
-const ProfileScreen = TabNavigator({
+const ProfileNavigator = TabNavigator({
     Local: {screen: ProfileLocalScreen},
     Facebook: {screen: ProfileFacebookScreen},
 });
 
-ProfileScreen.navigationOptions = ({navigation}) => ({
-    title: `Profiles`,
+ProfileNavigator.navigationOptions = ({navigation}) => ({
+    title: `Profile Screen`,
 });
 
-export default ProfileScreen;
+export default class ProfileScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.screenProps = {
+            user: this.props.navigation.state.params.user,
+        };
+    }
+
+    render() {
+        return (
+            <ProfileNavigator screenProps={this.screenProps}/>
+        );
+    }
+}
