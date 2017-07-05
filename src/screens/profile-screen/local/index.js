9e85c5416a500bc285cce69b33e1ce401ff69957
fsarachu <fsarachu@gmail.com>
2017-07-05 19:51:47 -0300

    Use JSON tree viewer

--- src/screens/profile-screen/local/index.js
+++ src/screens/profile-screen/local/index.js
@@ -1,14 +1,18 @@
 /* @flow */
 
 import React, {Component} from "react";
+import JSONTree from 'react-native-json-tree'
 import {Text, View} from "react-native";
 import styles from "./styles";
 
 export default class ProfileLocalScreen extends Component {
     render() {
+        const userData = this.props.screenProps.user.local;
+
         return (
             <View style={styles.container}>
-                <Text>Local Profile</Text>
+                <Text>Local profile</Text>
+                <JSONTree data={userData}/>
             </View>
         );
     }
