913f8320ed712a813159c62396ddca87d6b9d601
fsarachu <fsarachu@gmail.com>
2017-07-06 11:58:53 -0300

    Store facebook profile in component state and clean up code

--- src/screens/profile-screen/facebook/index.js
+++ src/screens/profile-screen/facebook/index.js
@@ -1,49 +1,70 @@
 /* @flow */
 
 import React, {Component} from "react";
-import {LoginButton, } from "react-native-fbsdk";
+import {LoginButton} from "react-native-fbsdk";
 import JSONTree from "react-native-json-tree";
 import {Alert, Text, View} from "react-native";
 import styles from "./styles";
 import UserService from "../../../services/user-service";
 
 export default class ProfileFacebookScreen extends Component {
-    render() {
-        const user = this.props.screenProps.user;
-        const userData = user.facebook;
-        const fbButton = (
-            <LoginButton
-                readPermissions={['public_profile', 'email']}
-                onLoginFinished={
-                    (error, result) => {
-                        if (error) {
-                            Alert.alert("Login failed with error: " + result.error);
-                        } else if (result.isCancelled) {
-                            Alert.alert("Login was cancelled");
-                        } else {
-                            user.facebook = result;
-                            UserService.save(user);
-                        }
-                    }
-                }
-                onLogoutFinished={() => Alert.alert("User logged out")}/>
-        );
 
-        if (!userData) {
-            return (
-                <View style={styles.container}>
-                    <Text>Facebook profile</Text>
-                    {fbButton}
-                </View>
-            );
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: this.props.screenProps.user,
+        };
+    }
+
+    onLogout() {
+        UserService.get()
+            .then((user) => {
+                user.facebook = null;
+                return UserService.save(user);
+            })
+            .then((user) => {
+                this.setState((prev) => ({user}));
+            })
+            .catch((e) => Alert.alert(e));
+    }
+
+    onLogin(error, result) {
+        if (error) {
+            Alert.alert("Login failed with error: " + result.error);
+        } else if (result.isCancelled) {
+            Alert.alert("Login was cancelled");
         } else {
-            return (
-                <View style={styles.container}>
-                    <Text>Facebook profile</Text>
-                    {fbButton}
-                    <JSONTree data={userData}/>
-                </View>
-            );
+            UserService.get()
+                .then((user) => {
+                    user.facebook = result;
+                    return UserService.save(user);
+                })
+                .then((user) => {
+                    this.setState((prev) => ({user}));
+                })
+                .catch((e) => Alert.alert(e));
         }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+
+                <Text>Facebook profile</Text>
+
+                <LoginButton
+                    readPermissions={['public_profile', 'email']}
+                    onLoginFinished={this.onLogin.bind(this)}
+                    onLogoutFinished={this.onLogout.bind(this)}
+                />
+
+                {this.state.user && this.state.user.facebook ? (
+                    <JSONTree data={this.state.user.facebook}/>
+                ) : (
+                    null
+                )}
+
+            </View>
+        );
     }
 }
